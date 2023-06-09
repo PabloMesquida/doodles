8589b8e538aa789fc904ab7f8b41384313a79ecc
Pablo Mesquida <pablomesquida67@gmail.com>
2023-06-09 22:28:07 +0200

    get-context-2d-log

--- src/components/AddEditNoteDialog.tsx
+++ src/components/AddEditNoteDialog.tsx
@@ -53,9 +53,13 @@ const AddEditNoteDialog = ({ noteToEdit, onDismiss, onNoteSaved }: AddEditNoteDi
     try {
       if (canvas) {
         const context = canvas.getContext("2d");
+        console.log("CONTEXT: ", context);
         const imageData = context?.getImageData(0, 0, canvas.width, canvas.height);
+        console.log("IMAGEDATE: ", imageData);
         const blob = new Blob([imageData as unknown as BlobPart], { type: "image/png" });
+        console.log("BLOB", blob);
         const file = new File([blob], "filename.png", { type: "image/png" });
+        console.log("FILE", file);
         const fileRandomName = generateRandomName();
         uploadImage({ file, fileName: fileRandomName });
       }
