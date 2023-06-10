5d538698c2bcaea6415a68a5fb590b4644f2e0bf
Pablo Mesquida <pablomesquida67@gmail.com>
2023-06-10 16:55:16 +0200

    noset

--- src/components/AddEditNoteDialog.tsx
+++ src/components/AddEditNoteDialog.tsx
@@ -9,6 +9,33 @@ import TextInputField from "./forms/TextInputField";
 import { generateRandomName } from "../utils/generateRandomName";
 import { useCanvasContext } from "../context/CanvasContext";
 
+import { ReactP5Wrapper, P5CanvasInstance } from "react-p5-wrapper";
+
+export const Doodle = () => {
+  // const { setCanvas } = useCanvasContext();
+  console.log("DOOLE");
+  const sketch = (p5: P5CanvasInstance) => {
+    p5.setup = () => {
+      p5.createCanvas(400, 400);
+      p5.background(255);
+    };
+
+    p5.draw = () => {
+      p5.fill(0);
+      p5.stroke(0);
+      p5.strokeWeight(25);
+      if (p5.mouseIsPressed === true) {
+        p5.line(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY);
+        // if (setCanvas) {
+        //   setCanvas(p5);
+        // }
+      }
+    };
+  };
+
+  return <ReactP5Wrapper sketch={sketch} />;
+};
+
 interface AddEditNoteDialogProps {
   noteToEdit?: Note;
   onDismiss: () => void;
