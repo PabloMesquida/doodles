1a644eee675fce2171512af3ee793d8818719d9b
Pablo Mesquida <pablomesquida67@gmail.com>
2023-06-10 12:38:07 +0200

    doodle

--- src/components/AddEditNoteDialog.tsx
+++ src/components/AddEditNoteDialog.tsx
@@ -6,7 +6,34 @@ import { useForm } from "react-hook-form";
 import { NoteInput } from "../network/notes_api";
 import TextInputField from "./forms/TextInputField";
 import { ReactP5Wrapper, P5CanvasInstance } from "react-p5-wrapper";
-import { generateRandomName } from "../utils/generateRandomName";
+// import { generateRandomName } from "../utils/generateRandomName";
+
+interface DoodleProps {
+  canvasRef: React.RefObject<HTMLDivElement>;
+}
+
+const Doodle = ({ canvasRef }: DoodleProps) => {
+  const sketch = (p5: P5CanvasInstance) => {
+    p5.setup = () => {
+      p5.createCanvas(400, 400).parent(canvasRef.current);
+      // canvasRef.current?.appendChild(canvas.canvas);
+      //  console.log("-- CANVAS ---", canvas);
+      console.log("--- CANVAS REF ---", canvasRef.current);
+
+      p5.background(255);
+    };
+
+    p5.draw = () => {
+      p5.fill(0);
+      p5.stroke(0);
+      p5.strokeWeight(25);
+      if (p5.mouseIsPressed === true) {
+        p5.line(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY);
+      }
+    };
+  };
+  return <ReactP5Wrapper sketch={sketch} />;
+};
 
 interface AddEditNoteDialogProps {
   noteToEdit?: Note;
@@ -25,54 +52,33 @@ const AddEditNoteDialog = ({ noteToEdit, onDismiss, onNoteSaved }: AddEditNoteDi
     },
   });
 
-  //const [canvasDataURL, setCanvasDataURL] = useState<string | null>(null);
-  let canvasRef = useRef<HTMLCanvasElement>(null);
-
-  function sketch(p5: P5CanvasInstance) {
-    p5.setup = () => {
-      const canvas = p5.createCanvas(400, 400);
-      canvasRef = canvas.canvas;
-      console.log("-- CANVAS ---", canvas);
-      console.log("--- CANVAS REF ---", canvasRef.current);
-
-      p5.background(255);
-    };
-
-    p5.draw = () => {
-      p5.fill(0);
-      p5.stroke(0);
-      p5.strokeWeight(25);
-      if (p5.mouseIsPressed === true) {
-        p5.line(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY);
-      }
-    };
-  }
+  const canvasRef = useRef<HTMLDivElement>(null);
 
   async function onSubmit(input: NoteInput) {
     const canvas = canvasRef.current;
     console.log("CANVAS", canvas);
 
     if (canvasRef.current) {
-      const canvas = canvasRef.current;
-      const context = canvas.getContext("2d");
+      //   const canvas = canvasRef.current;
+      // const context = canvas.getContext("2d");
       console.log(
-        "Contenido del canvas:",
-        context?.getImageData(0, 0, canvas.width, canvas.height)
+        "Contenido del canvas:"
+        //  context?.getImageData(0, 0, canvas.width, canvas.height)
       );
     }
 
     try {
       if (canvas) {
-        const blob = await new Promise<Blob | null>((resolve) => {
-          canvas.toBlob((value) => resolve(value), "image/png");
-        });
-        if (blob) {
-          const file = new File([blob], "filename.png", { type: "image/png" });
-          const fileRandomName = generateRandomName();
-          uploadImage({ file, fileName: fileRandomName });
-        } else {
-          console.error("Error al generar el Blob");
-        }
+        // const blob = await new Promise<Blob | null>((resolve) => {
+        //   canvas.toBlob((value) => resolve(value), "image/png");
+        // });
+        // if (blob) {
+        //   const file = new File([blob], "filename.png", { type: "image/png" });
+        //   const fileRandomName = generateRandomName();
+        //   uploadImage({ file, fileName: fileRandomName });
+        // } else {
+        //   console.error("Error al generar el Blob");
+        // }
       }
     } catch (error) {
       console.error("Error al generar o cargar la imagen:", error);
@@ -93,39 +99,39 @@ const AddEditNoteDialog = ({ noteToEdit, onDismiss, onNoteSaved }: AddEditNoteDi
     }
   }
 
-  interface UploadImageParams {
-    file: File;
-    fileName: string;
-  }
+  // interface UploadImageParams {
+  //   file: File;
+  //   fileName: string;
+  // }
 
-  async function uploadImage({ file, fileName }: UploadImageParams): Promise<void> {
-    console.log("UPLOAD IMAGE", file, fileName);
-    const cloudName = "dq2hljnad";
-    const uploadPreset = "doodles-upload";
+  // async function uploadImage({ file, fileName }: UploadImageParams): Promise<void> {
+  //   console.log("UPLOAD IMAGE", file, fileName);
+  //   const cloudName = "dq2hljnad";
+  //   const uploadPreset = "doodles-upload";
 
-    const url = `https://api.cloudinary.com/v1_1/${cloudName}/upload`;
-    const formData: FormData = new FormData();
+  //   const url = `https://api.cloudinary.com/v1_1/${cloudName}/upload`;
+  //   const formData: FormData = new FormData();
 
-    formData.append("file", file);
-    formData.append("upload_preset", uploadPreset);
-    formData.append("public_id", fileName);
+  //   formData.append("file", file);
+  //   formData.append("upload_preset", uploadPreset);
+  //   formData.append("public_id", fileName);
 
-    try {
-      const response = await fetch(url, {
-        method: "POST",
-        body: formData,
-      });
+  //   try {
+  //     const response = await fetch(url, {
+  //       method: "POST",
+  //       body: formData,
+  //     });
 
-      if (response.ok) {
-        console.log("Archivo subido con éxito");
-        // Hacer algo con la respuesta de Cloudinary
-      } else {
-        console.error("Error al subir archivo:", response.statusText);
-      }
-    } catch (error) {
-      console.error("Error de red:", error);
-    }
-  }
+  //     if (response.ok) {
+  //       console.log("Archivo subido con éxito");
+  //       // Hacer algo con la respuesta de Cloudinary
+  //     } else {
+  //       console.error("Error al subir archivo:", response.statusText);
+  //     }
+  //   } catch (error) {
+  //     console.error("Error de red:", error);
+  //   }
+  // }
 
   return (
     <Modal show onHide={onDismiss}>
@@ -144,8 +150,9 @@ const AddEditNoteDialog = ({ noteToEdit, onDismiss, onNoteSaved }: AddEditNoteDi
             error={errors.title}
           />
         </Form>
-        <ReactP5Wrapper sketch={sketch} />
-        <canvas ref={canvasRef} style={{ display: "none" }} />
+        <Doodle canvasRef={canvasRef} />
+        {/* <ReactP5Wrapper sketch={sketch} />
+        <div ref={canvasRef} style={{ display: "none" }} /> */}
       </Modal.Body>
       <Modal.Footer>
         <Button type="submit" form="addEditNoteForm" disabled={isSubmitting}>
