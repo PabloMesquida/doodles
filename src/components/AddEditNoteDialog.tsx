4a4da2d9feaba20db7d47820f30ab4acf53f7fde
Pablo Mesquida <pablomesquida67@gmail.com>
2023-06-12 13:02:50 +0200

    Doodle-component

--- src/components/AddEditNoteDialog.tsx
+++ src/components/AddEditNoteDialog.tsx
@@ -1,34 +1,13 @@
 import * as NoteApi from "../network/notes_api";
-import { memo } from "react";
 import { Button, Form, Modal } from "react-bootstrap";
 import { Note } from "../models/note";
 import { useForm } from "react-hook-form";
 import { NoteInput } from "../network/notes_api";
-import { ReactP5Wrapper, P5CanvasInstance } from "react-p5-wrapper";
+import { P5CanvasInstance } from "react-p5-wrapper";
 import generateRandomName from "../utils/generateRandomName";
 import uploadImage from "../utils/uploadImage";
 import TextInputField from "./forms/TextInputField";
-
-const Doodle = memo(() => {
-  const sketch = (p5: P5CanvasInstance) => {
-    p5.setup = () => {
-      p5.createCanvas(400, 400).id("mycanvas");
-      p5.background(255, 0, 0);
-    };
-
-    p5.draw = () => {
-      p5.fill(0);
-      p5.stroke(0);
-      p5.strokeWeight(25);
-
-      if (p5.mouseIsPressed === true) {
-        p5.line(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY);
-      }
-    };
-  };
-
-  return <ReactP5Wrapper sketch={sketch} />;
-});
+import Doodle from "./Doodle";
 
 interface AddEditNoteDialogProps {
   noteToEdit?: Note;
@@ -79,38 +58,6 @@ const AddEditNoteDialog = ({ noteToEdit, onDismiss, onNoteSaved }: AddEditNoteDi
       alert(error);
     }
   }
-
-  // interface UploadImageParams {
-  //   file: File;
-  //   fileName: string;
-  // }
-
-  // async function uploadImage({ file, fileName }: UploadImageParams): Promise<void> {
-  //   const cloudName = "dq2hljnad";
-  //   const uploadPreset = "doodles-upload";
-
-  //   const url = `https://api.cloudinary.com/v1_1/${cloudName}/upload`;
-  //   const formData: FormData = new FormData();
-
-  //   formData.append("file", file);
-  //   formData.append("upload_preset", uploadPreset);
-  //   formData.append("public_id", fileName);
-
-  //   try {
-  //     const response = await fetch(url, {
-  //       method: "POST",
-  //       body: formData,
-  //     });
-
-  //     if (response.ok) {
-  //       console.log("Archivo subido con éxito");
-  //     } else {
-  //       console.error("Error al subir archivo:", response.statusText);
-  //     }
-  //   } catch (error) {
-  //     console.error("Error de red:", error);
-  //   }
-  // }
 
   return (
     <Modal show onHide={onDismiss}>
--- src/components/Doodle.tsx
+++ src/components/Doodle.tsx
@@ -1,17 +1,18 @@
+import { memo } from "react";
 import { ReactP5Wrapper, P5CanvasInstance } from "react-p5-wrapper";
 
-export const Doodle = () => {
-  console.log("DOOLE- PERON");
+const Doodle = memo(() => {
   const sketch = (p5: P5CanvasInstance) => {
     p5.setup = () => {
-      p5.createCanvas(400, 400);
-      p5.background(255);
+      p5.createCanvas(400, 400).id("mycanvas");
+      p5.background(255, 0, 0);
     };
 
     p5.draw = () => {
       p5.fill(0);
       p5.stroke(0);
       p5.strokeWeight(25);
+
       if (p5.mouseIsPressed === true) {
         p5.line(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY);
       }
@@ -19,4 +20,6 @@ export const Doodle = () => {
   };
 
   return <ReactP5Wrapper sketch={sketch} />;
-};
+});
+
+export default Doodle;
