4a1cb44b45bb224e0a20a984d560e7f9e3da7558
Pablo Mesquida <pablomesquida67@gmail.com>
2023-06-14 15:02:05 +0200

    doodle-line-color

--- src/components/Doodle.tsx
+++ src/components/Doodle.tsx
@@ -5,12 +5,11 @@ const Doodle = memo(() => {
   const sketch = (p5: P5CanvasInstance) => {
     p5.setup = () => {
       p5.createCanvas(600, 600).id("p5canvas");
-      p5.background(252, 250, 250);
+      p5.background(235, 235, 218);
     };
 
     p5.draw = () => {
-      p5.fill(0);
-      p5.stroke(0);
+      p5.stroke(10, 10, 10);
       p5.strokeWeight(25);
 
       if (p5.mouseIsPressed === true) {
