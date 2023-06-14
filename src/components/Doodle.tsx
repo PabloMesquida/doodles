bfbb1bc7ee5e53799d3132bf362aa7de2f1e2246
Pablo Mesquida <pablomesquida67@gmail.com>
2023-06-14 15:06:43 +0200

    doodle-line-color3

--- src/components/Doodle.tsx
+++ src/components/Doodle.tsx
@@ -4,13 +4,13 @@ import { ReactP5Wrapper, P5CanvasInstance } from "react-p5-wrapper";
 const Doodle = memo(() => {
   const sketch = (p5: P5CanvasInstance) => {
     p5.setup = () => {
-      p5.createCanvas(400, 400).id("p5canvas");
+      p5.createCanvas(380, 380).id("p5canvas");
       p5.background(235, 235, 218);
     };
 
     p5.draw = () => {
-      p5.stroke(15, 15, 15);
-      p5.strokeWeight(25);
+      p5.stroke(16, 16, 16);
+      p5.strokeWeight(12);
 
       if (p5.mouseIsPressed === true) {
         p5.line(p5.mouseX, p5.mouseY, p5.pmouseX, p5.pmouseY);
