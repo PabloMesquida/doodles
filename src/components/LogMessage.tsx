c23d7652e0abdbdb39ca9ef12ff1ede9ac87b84a
Pablo Mesquida <pablomesquida67@gmail.com>
2023-06-16 17:40:43 +0200

    nav-btn-phrases

--- src/components/LogMessage.tsx
+++ src/components/LogMessage.tsx
@@ -4,7 +4,7 @@ import styles from "../styles/NotesPage.module.css";
 export const LogMessage = () => {
 	const phrases = [
 		"Hop in and log in to share your awesome doodles!",
-		"Hey there! Sign in to show off your cool doodles!",
+		"Hey there! log in to show off your cool doodles!",
 		"Time to log in and unleash your doodling magic!",
 		"Ready to join the fun? Login and share your amazing doodles!",
 		"Hey, doodle enthusiast! Sign in and let your creativity shine!",
@@ -12,7 +12,7 @@ export const LogMessage = () => {
 		"Calling all doodlers! Sign in and share your masterpieces!",
 		"Get doodlin'! Log in and display your doodle awesomeness!",
 		"Login and be part of the doodle-loving community!",
-		"Sign in and let your doodles spread some smiles!",
+		"Login and let your doodles spread some smiles!",
 	];
 
 	const randomIndex = Math.floor(Math.random() * phrases.length);
